00741534a0c594cb918847a37b3180ef8a9c2327
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-11-20 22:45:02 +0700

    Repush Deployment

--- inventori/settings.py
+++ inventori/settings.py
@@ -14,6 +14,34 @@ from pathlib import Path
 import os
 import environ
 
+CSRF_TRUSTED_ORIGINS = ['https://kenichi-komala-tutorial.pbp.cs.ui.ac.id']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = "unsafe-none"
+
+CORS_ALLOWED_ORIGINS = [
+    "https://kenichi-komala-tutorial.pbp.cs.ui.ac.id",
+]
+
+LOGIN_REDIRECT_URL = 'http://kenichi-komala-tutorial.pbp.cs.ui.ac.id/'  # yg lu pengen redir kalo misalny selesai loggin
+
+CORS_ALLOW_HEADERS=[
+    'accept',
+    'ccept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user_agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'acces-control-allow-origin'
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
